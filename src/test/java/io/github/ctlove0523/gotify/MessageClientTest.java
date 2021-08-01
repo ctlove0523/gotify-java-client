bbc2afcd415e7437cc2ac98ba62fcccff44ad2a5
ctlove0523 <478309639@qq.com>
2021-08-01 10:20:04 +0800

    add unit test

--- src/test/java/io/github/ctlove0523/gotify/MessageClientTest.java
+++ src/test/java/io/github/ctlove0523/gotify/MessageClientTest.java
@@ -1,5 +1,6 @@
 package io.github.ctlove0523.gotify;
 
+import java.util.List;
 import java.util.Random;
 
 import io.github.ctlove0523.gotify.message.Message;
@@ -11,9 +12,9 @@ import org.junit.Test;
 
 public class MessageClientTest {
 
-	private MessageMockServer server = new MessageMockServer();
+	private final MessageMockServer server = new MessageMockServer();
 
-	private Random random = new Random();
+	private final Random random = new Random();
 
 	@Before
 	public void startServer() throws Exception {
@@ -48,16 +49,145 @@ public class MessageClientTest {
 
 
 		MessageClient messageClient = newGotifyClient().getMessageClient();
-		Result<PagedMessages,ResponseError> response = messageClient.getAppMessages(appId);
+		Result<PagedMessages, ResponseError> response = messageClient.getAppMessages(appId);
 
 		Assert.assertNotNull(response);
 		Assert.assertTrue(response.isSuccessful());
 
 		PagedMessages pagedMessages = response.result();
 		Assert.assertNotNull(pagedMessages);
-		Assert.assertEquals(2,pagedMessages.getMessages().size());
+		Assert.assertEquals(2, pagedMessages.getMessages().size());
 	}
 
+	@Test
+	public void test_deleteAppMessages() {
+		Integer appId = random.nextInt();
+
+		Message message1 = new Message();
+		message1.setAppid(appId);
+		message1.setMessage("message1");
+		message1.setTitle("title1");
+		message1.setId(random.nextInt());
+		message1.setPriority(random.nextInt());
+
+		Message message2 = new Message();
+		message2.setAppid(appId);
+		message2.setMessage("message1");
+		message2.setTitle("title1");
+		message2.setId(random.nextInt());
+		message2.setPriority(random.nextInt());
+
+		server.addMessage(message1);
+		server.addMessage(message2);
+
+		MessageClient messageClient = newGotifyClient().getMessageClient();
+
+		Result<Boolean, ResponseError> result = messageClient.deleteAppMessages(appId);
+		Assert.assertTrue(result.isSuccessful());
+
+		List<Message> messages = server.getAppMessage(appId);
+		Assert.assertTrue(messages.isEmpty());
+	}
+
+	@Test
+	public void test_getMessages() {
+		Integer appId = random.nextInt();
+
+		Message message1 = new Message();
+		message1.setAppid(appId);
+		message1.setMessage("message1");
+		message1.setTitle("title1");
+		message1.setId(random.nextInt());
+		message1.setPriority(random.nextInt());
+
+		Message message2 = new Message();
+		message2.setAppid(appId);
+		message2.setMessage("message1");
+		message2.setTitle("title1");
+		message2.setId(random.nextInt());
+		message2.setPriority(random.nextInt());
+
+		server.addMessage(message1);
+		server.addMessage(message2);
+
+		MessageClient messageClient = newGotifyClient().getMessageClient();
+
+		Result<PagedMessages, ResponseError> result = messageClient.getMessages();
+		Assert.assertTrue(result.isSuccessful());
+
+		List<Message> messages = result.result().getMessages();
+		Assert.assertEquals(2, messages.size());
+	}
+
+	@Test
+	public void test_deleteMessages() {
+		Integer appId = random.nextInt();
+
+		Message message1 = new Message();
+		message1.setAppid(appId);
+		message1.setMessage("message1");
+		message1.setTitle("title1");
+		message1.setId(random.nextInt());
+		message1.setPriority(random.nextInt());
+
+		Message message2 = new Message();
+		message2.setAppid(appId);
+		message2.setMessage("message1");
+		message2.setTitle("title1");
+		message2.setId(random.nextInt());
+		message2.setPriority(random.nextInt());
+
+		server.addMessage(message1);
+		server.addMessage(message2);
+
+		List<Message> messages = server.getMessages();
+		Assert.assertEquals(2, messages.size());
+
+		MessageClient messageClient = newGotifyClient().getMessageClient();
+
+		Result<Boolean, ResponseError> result = messageClient.deleteMessages();
+		Assert.assertTrue(result.isSuccessful());
+
+		messages = server.getMessages();
+		Assert.assertTrue(messages.isEmpty());
+
+	}
+
+	@Test
+	public void test_deleteMessage() {
+		Integer appId = random.nextInt();
+
+		Integer messageId1 = random.nextInt();
+		Message message1 = new Message();
+		message1.setAppid(appId);
+		message1.setMessage("message1");
+		message1.setTitle("title1");
+		message1.setId(messageId1);
+		message1.setPriority(random.nextInt());
+
+		Integer messageId2 = random.nextInt();
+		Message message2 = new Message();
+		message2.setAppid(appId);
+		message2.setMessage("message1");
+		message2.setTitle("title1");
+		message2.setId(messageId2);
+		message2.setPriority(random.nextInt());
+
+		server.addMessage(message1);
+		server.addMessage(message2);
+
+		List<Message> messages = server.getMessages();
+		Assert.assertEquals(2, messages.size());
+
+		MessageClient messageClient = newGotifyClient().getMessageClient();
+
+		Result<Boolean, ResponseError> result = messageClient.deleteMessage(messageId1);
+		Assert.assertTrue(result.isSuccessful());
+
+		messages = server.getMessages();
+		Assert.assertEquals(1, messages.size());
+
+	}
 
 	private GotifyClient newGotifyClient() {
 		Credential credential = new BasicCredential.Builder()
--- src/test/java/io/github/ctlove0523/gotify/MessageMockServer.java
+++ src/test/java/io/github/ctlove0523/gotify/MessageMockServer.java
@@ -1,5 +1,6 @@
 package io.github.ctlove0523.gotify;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,12 +26,14 @@ public class MessageMockServer {
 	public int port() {
 		return server.getPort();
 	}
+
 	public void stop() throws Exception {
 		if (Objects.nonNull(server)) {
 			server.shutdown();
 			appMessages.clear();
 		}
 	}
+
 	public void start() throws Exception {
 		this.server = new MockWebServer();
 		server.setDispatcher(new Dispatcher() {
@@ -58,6 +61,57 @@ public class MessageMockServer {
 
 					return response;
 				}
+
+				// 删除应用下的消息
+				if (method.equals("DELETE") && path.contains("/application")) {
+					System.out.println("begin to delete app message");
+					Integer appId = getAppId(request.getRequestUrl());
+					appMessages.remove(appId);
+					response.setResponseCode(200);
+					return response;
+				}
+
+				// 查询消息
+				if (method.equals("GET") && path.contains("/message")) {
+					System.out.println("begin to get all message");
+					List<Message> messages = new ArrayList<>();
+					appMessages.forEach((integer, messageList) -> messages.addAll(messageList));
+					Paging paging = new Paging();
+					paging.setSize(messages.size());
+
+					PagedMessages pagedMessages = new PagedMessages();
+					pagedMessages.setMessages(messages);
+					pagedMessages.setPaging(paging);
+
+					response.setBody(JacksonUtil.object2String(pagedMessages));
+
+					return response;
+				}
+
+				// 创建消息
+				if (method.equals("POST") && path.contains("/message")) {
+					System.out.println("begin to create message");
+					String content = request.getBody().readString(Charset.defaultCharset());
+					Message message = JacksonUtil.string2Object(content, Message.class);
+					addMessage(message);
+				}
+
+				// 删除所有消息
+				if (method.equals("DELETE") && path.contains("/message") && !path.contains("/message/")) {
+					System.out.println("begin to delete all message");
+					System.out.println(path);
+					appMessages.clear();
+					response.setResponseCode(200);
+					return response;
+				}
+
+				// 删除指定ID的消息
+				if (method.equals("DELETE") && path.contains("/message/")) {
+					Integer messageId = Integer.parseInt(path.replace("/message/", ""));
+					System.out.println("begin to delete message with id " + messageId);
+					deleteMessage(messageId);
+				}
+
 				return response;
 			}
 		});
@@ -79,6 +133,23 @@ public class MessageMockServer {
 	public void addMessages(List<Message> messages) {
 		for (Message message : messages) {
 			addMessage(message);
+		}
+	}
+
+	public List<Message> getAppMessage(Integer appId) {
+		return appMessages.getOrDefault(appId, new ArrayList<>());
+	}
+
+	public List<Message> getMessages() {
+		List<Message> messages = new ArrayList<>();
+		appMessages.forEach((integer, messageList) -> messages.addAll(messageList));
+
+		return messages;
+	}
+
+	private void deleteMessage(Integer id) {
+		for (Integer appId : appMessages.keySet()) {
+			appMessages.get(appId).removeIf(message -> message.getId().equals(id));
 		}
 	}
 }
